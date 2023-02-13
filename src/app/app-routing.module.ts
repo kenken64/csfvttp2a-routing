806cff0f8eaf97b66ba701a9d041dba607c5a220
KENNETH PHANG <bunnyppl@hotmail.com>
2023-02-13 12:00:28 +0800

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './components/customer.component';
+import { ElephantComponent } from './components/elephant.component';
+import { HomeComponent } from './components/home.component';
+import { TrexComponent } from './components/trex.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path: 'elephant', component: ElephantComponent },
+  { path: 'trex', component: TrexComponent },
+  { path: 'customer/:custId', component: CustomerComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ElephantComponent } from './components/elephant.component';
+import { TrexComponent } from './components/trex.component';
+import { HomeComponent } from './components/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CustomerComponent } from './components/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElephantComponent,
+    TrexComponent,
+    HomeComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
